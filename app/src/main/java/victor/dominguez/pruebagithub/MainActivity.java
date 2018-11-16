00bb5be911f127852888e806e9d81aee4b2b2079
victor.dominguez.roc <Mallorca95>
2018-11-16 12:55:29 +0100

    Algunos cambios

--- app/src/main/java/victor/dominguez/pruebagithub/MainActivity.java
+++ app/src/main/java/victor/dominguez/pruebagithub/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Hago un comentario
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
